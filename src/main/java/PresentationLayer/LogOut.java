66e03b6879bfc5c550971db962970368190c9feb
Phillip Andersen <phillip_andersen@hotmail.com>
2020-04-01 13:23:25 +0200

    nyt

--- src/main/java/PresentationLayer/LogOut.java
+++ src/main/java/PresentationLayer/LogOut.java
@@ -6,17 +6,24 @@ import FunctionLayer.LoginSampleException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class LogOut extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
+
+
         String email = String.valueOf(request.getSession().getAttribute("email"));
+
         int customer_id = CustomerMapper.getCustomerId(email);
+
         // Når man logger ud sørger vi for at alle orderlines bliver false
         OrderLineMapper.makeOrderLineInActive(customer_id);
+        request.getSession().setAttribute("email", null);
 
         request.getSession().invalidate();
 
+
         return "index";
-                }
-                }
+    }
+}
